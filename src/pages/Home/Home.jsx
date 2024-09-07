669ad6e466dd81cf5c84c35ea7dd9f6239319cd4
007killer2 <savenko.artem@gmx.de>
2024-09-07 14:51:15 +0200

    Homework18

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import {Navbar, Banner, Cards, Footer} from "../../components";
+
+
+export const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Banner />
+            <Cards />
+            <Footer />
+        </div>
+    )
+}
